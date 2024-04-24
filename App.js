0e80d1a15826e1e606801a7ac28c27178d914144
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-04-23 19:25:52 -0500

    calendar react native

--- App.js
+++ App.js
@@ -1,11 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Agenda } from "react-native-calendars";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import moment from "moment";
 
 export default function App() {
+  const formatearFecha = () => {
+    const moment = require("moment");
+    const fehaHoy = new Date();
+    const fechaFormateada = moment(fehaHoy).format("YYYY-MM-DD");
+    return fechaFormateada
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Agenda
+        selected={formatearFecha()}
+        futureScrollRange={12}
+        pastScrollRange={12}
+        items={{
+          "2024-03-26": [
+            {
+              name: "Meeting 1",
+              data: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. ",
+            },
+          ],
+          "2024-03-28": [
+            {
+              name: "Meeting 2",
+              data: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. ",
+            },
+          ],
+          "2024-03-29": [
+            {
+              name: "Meeting 3",
+              data: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. ",
+            },
+          ],
+          "2024-03-30": [
+            {
+              name: "Meeting 4",
+              data: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. ",
+            },
+          ],
+          "2024-03-31": [
+            {
+              name: "Meeting 5",
+              data: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. ",
+            },
+          ],
+          "2024-03-25": [
+            {
+              name: "Meeting 6",
+              data: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. ",
+            },
+          ],
+        }}
+        renderItem={(item, isFirst) => (
+          <TouchableOpacity style={styles.item}>
+            <Text style={styles.itemText}>{item.name}</Text>
+            <Text style={styles.itemText}>{item.data}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
@@ -13,8 +69,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    marginVertical:20
+  },
+  item: {
+    backgroundColor: "lightblue",
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 25,
+    paddingBottom: 20,
+  },
+  itemText: {
+    color: "black",
+    fontSize: 16,
   },
 });
